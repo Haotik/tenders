94e5e0b6912b23da6c4f152c930e137f1240cc26
Haotik <korehovrabota@yandex.ru>
2022-04-03 12:55:25 +0300

    Настройка левого меню под новые группы
    Слетело после мердж

--- application/views/header.php
+++ application/views/header.php
@@ -1082,7 +1082,7 @@
                         <li><a href="/responses/">Отзывы</a></li>
                     </ul>
                     <?php
-                } elseif ($group_id == 2 || $group_id == 3) {
+                } elseif ($group_id == 2 || $group_id == 3 || $group_id == 5 || $group_id == 6 || $group_id ==7) {
                     ?>
                     <ul>
                         <li><span>Инструкция</span></li>
